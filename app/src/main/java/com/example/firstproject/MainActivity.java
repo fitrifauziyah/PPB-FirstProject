fb9a3da733d372d36dda0e4a49b34e722af82ec2
fitrifauziyah <fitrifauziyah15@gmail.com>
2022-02-19 23:13:25 +0700

    First Project

---
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.firstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "oke mantap", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
